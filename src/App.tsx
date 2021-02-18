73fbdf172c73fe74e407687709b3498c5fc76554
Hossein Salehi <hossein@sisuwellness.com>
2021-02-19 10:43:35 +1100

    Add dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import { createMuiTheme, responsiveFontSizes, ThemeProvider } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
 
-let theme = createMuiTheme();
+let theme = createMuiTheme({
+    palette: {
+        type: "dark"
+    }
+});
 theme = responsiveFontSizes(theme);
 
 const App = () => {
     return (
         <ThemeProvider theme={theme}>
-            <div>DDUI</div>
+            <CssBaseline />
+            <Typography style={{ marginTop: 50 }}>Text should be white, background should be dark</Typography>
+            <AppBar color="inherit">
+                <Typography variant="h6">App bar background should be dark!</Typography>
+            </AppBar>
         </ThemeProvider>
     );
 };
