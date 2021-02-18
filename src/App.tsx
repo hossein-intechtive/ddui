406daebb0d9a8ef3d7f9cd007833e9cb10645d5b
Hossein Salehi <hossein@sisuwellness.com>
2021-02-19 10:33:23 +1100

    Add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
-import {
-  createMuiTheme,
-  responsiveFontSizes,
-  ThemeProvider,
-} from "@material-ui/core/styles";
+import { createMuiTheme, responsiveFontSizes, ThemeProvider } from "@material-ui/core/styles";
 
 let theme = createMuiTheme();
 theme = responsiveFontSizes(theme);
 
 const App = () => {
-  return (
-    <ThemeProvider theme={theme}>
-      {" "}
-      <div>DDUI</div>
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+            <div>DDUI</div>
+        </ThemeProvider>
+    );
 };
 
 export default App;
